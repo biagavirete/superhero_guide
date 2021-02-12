a96c0b014d769799fdb74215dcdb364a17df3435
biagavirete <biagavirete@gmail.com>
2021-02-12 19:11:59 -0300

    feat: use axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
-// import env from "react-dotenv";
+import axios from 'axios';
 
-// const api = 'https://kitsu.io/api/edge/';
+// const api = axios.create({
+//     baseURL: 'https://superhero-search.p.rapidapi.com/',
+//     params: {hero: 'batman'},
+//     headers: {
+//         'x-rapidapi-key': '56c276be8emsha7d65ae357f8af8p1aa1b9jsnc8889a939d93',
+//         'x-rapidapi-host': 'superhero-search.p.rapidapi.com'
+//       }
+//   });
 
-const api = `https://superheroapi.com/api/10159142286944414`
+const api = axios.create({
+    baseURL: 'https://superheroapi.com/api/10159142286944414'
+})
 
 export default api;
