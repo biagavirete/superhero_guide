0d621160a26cc86454317607e946801ed831a84e
biagavirete <biagavirete@gmail.com>
2021-02-12 19:10:54 -0300

    feat: create home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './styles.css';
+import SearchInput from '../../components/SearchInput';
+
+const Home = () => {
+  const [data, setData] = useState({});
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (text) {
+      async function fetchMyAPI() {
+        api.get(`/search/${text}`)
+          .then(response => setData(response.data))
+
+        console.log(data.results)
+      }
+      try {
+        fetchMyAPI()
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text])
+
+  return (
+    <div className="App">
+      <h1>Superhero Guide</h1>
+      <p>Superhero search application using the <a href="https://superheroapi.com/" target="_blank">SuperHero API</a>.</p>
+      <SearchInput value={text} onChange={(search) => setText(search)} />
+
+      {text && !data.results && (
+        <span>Loading...</span>
+      )}
+
+      {data.results && (
+        <div className="container">
+          <ul className="characters-list">
+            {data.results.map((character) => (
+              <li key={character.id}>
+                <div className="card">
+                  <div className="content">
+                    <h2>{character.id}</h2>
+                    <h3>{character.name}</h3>
+                    <img src={character.image.url} alt={character.name} />
+                    <Link to={`/${character.id}`}>Read more</Link>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
